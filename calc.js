1f526fa1608678249f52e8a8a5236939a9be381e
Rachel Barkanic <rachel.barkanic@gmail.com>
2022-08-23 18:54:26 -0700

    initial commit

--- calc.js
+++ calc.js
@@ -15,11 +15,22 @@ reader.question("What would you like to calculate?", function(input){
 	const num2 = Number(tokens[2]);
 
 	if (mathSymbol === "+") {
-		console.log ("addition",num1 + num2)
+		console.log ('addition',num1 + num2)
 	}
     if (mathSymbol === "-") {
 		console.log ('subtraction' ,num1 - num2)
 	}
+	if (mathSymbol === "*") {
+		console.log ('multiplication' ,num1 * num2)
+	}
+	if (mathSymbol === "/") {
+		console.log ('division' ,num1 / num2)
+	}
+	if (mathSymbol === "sqrt") {
+		console.log ('square root' ,Math.sqrt(num1))
+	}
+
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
